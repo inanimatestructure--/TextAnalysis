f3d11a5aeb93e50ed863a63c1bcf358affb9b2e8
inanimatestructure-- <jakehollis425@gmail.com>
2021-08-02 17:46:42 -0400

    good stylings

--- phrase_searcher.rb
+++ phrase_searcher.rb
@@ -73,10 +73,10 @@ class Phrase
 
         #textfields && listboxes
 
-        file_phrases = TkText.new(content) {width 40; height 15}
+        file_phrases = TkText.new(content) {width 70; height 14}
 
         file_text = TkListbox.new(content) do
-            width 20    
+            width 30   
             height 15
             borderwidth 1
             font TkFont.new('helvetica 11')
@@ -91,21 +91,6 @@ class Phrase
 
         ys = Tk::Tile::Scrollbar.new(content) {orient 'vertical'; command proc{|*args| phrase_text.yview(*args);}}
         phrase_text['yscrollcommand'] = proc{|*args| ys.set(*args);}
-        ## canvas
-
-        file_chart = TkCanvas.new(content) { width 500; height 700 }
-
-        TkcLine.new(file_chart,50,250,50,50,'width'=>2)
-        TkcLine.new(file_chart,50,250,250,250,'width'=>2)
-
-        1.upto(10) do |i|
-            x = 50 + (i*20)
-            y = 250 - (i *20)
-            TkcLine.new(file_chart,45,y,50,y)
-            TkcText.new(file_chart,46,y,'text'=>10*i,'anchor'=>'e')
-        end
-
-
 
         # action items on click
         file_delete = Proc.new {
@@ -132,24 +117,26 @@ class Phrase
             end
 
             phrase_array = self.find_phrases(@doc_files)
-            puts phrase_array
             phrase_array.each do |pi|
-                phrase_text.insert 'end', "Document: " + pi["document"] + "\nPhrase: " + pi["phrase"] + "\nOccurs: " + pi["count"].to_s + " times\n\n" 
+                phrase_text.insert 'end', "Document: " + pi["document"] + "\nPhrase: " + pi["phrase"] + "\nOccurs: " + pi["count"].to_s + "\n\n"
             end
-
         }
 
-        file_title.grid :column => 0, :row => 0, :rowspan => 2, :sticky => 'nwes'
-        phrase_title.grid     :column => 1, :row => 1
-        file_upload.grid    :column => 0, :row => 0, :pady => 5
-        file_delete_btn.grid    :column => 0, :row => 2,:padx => 5, :sticky => 'w'
-        file_text.grid      :column => 0, :row => 1, :rowspan => 2, :padx => 20
-        phrase_text.grid      :column => 0, :row => 3, :sticky => 'nwes'
-        file_phrases.grid     :column => 1, :row => 2, :sticky => 'w', :padx => 20
-        file_chart.grid      :column => 4, :row => 0, :rowspan => 4, :sticky => 'nwes', :padx => 10
-        ys.grid               :column => 1, :row => 3, :sticky => 'ns'
-        file_search.grid      :column => 0, :row => 4, :pady => 10
+        # row 1 
+        file_title.grid :column => 0, :row => 0, :columnspan => 2, :sticky => 'w'
+        file_upload.grid    :column => 2, :row => 0, :padx => 3
+        file_delete_btn.grid    :column => 3, :row => 0, :padx => 3
+        phrase_title.grid     :column => 4, :row => 0,:columnspan => 3, :sticky => 'w', :padx => 5
+       
+        # row 2 and 3
+        file_text.grid      :column => 0, :row => 1, :rowspan => 2, :columnspan => 3, :sticky => 'w'
+        file_phrases.grid     :column => 4, :row => 1, :rowspan => 2, :columnspan => 4, :padx => 5
 
+        # row 4
+        file_search.grid      :column => 0, :row => 3, :sticky => 'w', :padx => 3
+        phrase_text.grid      :column => 0, :row => 4, :rowspan => 3, :columnspan => 6, :pady => 5, :sticky => 'w'
+        ys.grid               :column => 5, :row => 4, :rowspan => 3, :sticky => 'ns'
+        
         file_delete_btn.command = file_delete
         file_upload.command = file_click
         file_search.command = search_click
